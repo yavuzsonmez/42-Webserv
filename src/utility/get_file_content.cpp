dfd1a3596fb00f0c48728247cf383c964b854d1a
pweinstock <37242263+pweinstock@users.noreply.github.com>
2022-07-22 19:02:15 +0200

    get_file_content.cpp

---
+++ src/utility/get_file_content.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
+
+std::string get_file_content(std::string path)
+{
+	std::ifstream f(path); //taking file as inputstream
+	std::string str;
+	if(f) {
+		std::ostringstream ss;
+		ss << f.rdbuf(); // reading data
+		str = ss.str();
+	}
+	return (str);
+}
+
+int	main(int argc, char**argv)
+{
+	(void)argc;
+	std::cout << get_file_content(argv[1]) << std::endl;
+	return (0);
+}
