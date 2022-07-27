e804936b83efde3330e8c6a6da23e7b4552900f0
Yavuz Sönmez <contact@yavuzsonmez.com>
2022-07-27 16:05:05 +0200

    restructure Request class

--- inc/request.hpp
+++ inc/request.hpp
@@ -3,12 +3,11 @@
 
 # include <iostream>
 # include <string>
-# include <map>
+# include <vector>
 # include <sstream>
 # include "colors.hpp"
 
 enum method { GET, POST, DELETE, UNKNOWN };
-enum protocol { HTTP, OTHER };
 
 class Request
 {
@@ -21,51 +20,47 @@ class Request
 		~Request();
 
 		//getters
-		std::string getMethod() const;
-		std::string	getProtocol() const;
-		std::string getDomain() const;
-		std::string getPort() const;
-		std::string getScriptname() const;
-		std::string getPath() const;
-		std::string getQuerystring() const;
-		std::string getFragment() const;
-		std::string getHttpversion() const;
-		std::map <std::string, std::string> getHeaders() const;
-		std::string getBody() const;
+		std::pair<method, bool>					getMethod() const;
+		std::pair<std::string, bool>			getProtocol() const;
+		std::pair<std::string, bool>			getDomain() const;
+		std::pair<unsigned int, bool>			getPort() const;
+		std::pair<std::string, bool>			getScriptname() const;
+		std::pair<std::string, bool>			getPath() const;
+		std::pair<std::string, bool>			getQuerystring() const;
+		std::pair<std::string, bool>			getFragment() const;
+		std::pair<std::string, bool>			getHttpversion() const;
+		std::vector<std::pair<std::pair<std::string, bool>, std::pair<std::string, bool>>>
+												getHeaders() const;
+		std::pair<std::string, bool>			getBody() const;
 
 		//main parser
-		void parser(std::string &req);
+		void	parser(std::string &req);
 		//setters for parser
-		void setMethod(std::string &req);
-		void setProtocol(std::string &req);
-		void setDomain(std::string &req);
-		void setPort(std::string &req);
-		void setScriptname(std::string &req);
-		void setPath(std::string &req);
-		void setQuerystring(std::string &req);
-		void setFragment(std::string &req);
-		void setHttpversion(std::string &req);
-		void setHeaders(std::string &req);
-		void setBody(std::string &req);
+		void	setMethod(std::string &req);
+		void	setProtocol(std::string &req);
+		void	setDomain(std::string &req);
+		void	setPort(std::string &req);
+		void	setScriptname(std::string &req);
+		void	setPath(std::string &req);
+		void	setQuerystring(std::string &req);
+		void	setFragment(std::string &req);
+		void	setHttpversion(std::string &req);
+		void	setHeaders(std::string &req);
+		void	setBody(std::string &req);
 
 	private:
-
-		// Method
-		unsigned short	 _method; //method
-		// Url
-		unsigned short	 _protocol;
-		std::string		 _domain;
-		unsigned int	 _port;
-		std::string		 _scriptName;
-		std::string		 _path;
-		std::string		 _queryString;
-		std::string		 _fragment;
-		// Http version
-		std::string		 _httpVersion;
-		// Headers
-		std::map <std::string, std::string>	_headers;
-		// Body
-		std::string		_body;
+		std::pair<method, bool>				_method;
+		std::pair<std::string, bool>		_protocol;
+		std::pair<std::string, bool>		_domain;
+		std::pair<unsigned int, bool>		_port;
+		std::pair<std::string, bool>		_scriptName;
+		std::pair<std::string, bool>		_path;
+		std::pair<std::string, bool>		_queryString;
+		std::pair<std::string, bool>		_fragment;
+		std::pair<std::string, bool>		_httpVersion;
+		std::vector<std::pair<std::pair<std::string, bool>, std::pair<std::string, bool>>>
+											_headers;
+		std::pair<std::string, bool>		_body;
 };
 
 std::ostream &			operator<<( std::ostream & o, Request const & i );
--- src/parsing/request.cpp
+++ src/parsing/request.cpp
@@ -4,18 +4,32 @@
 /* **********************************************************/
 
 Request::Request() :
-	_method(UNKNOWN),
-	_protocol(HTTP),
-	_domain("/"),
-	_port(80),
-	_httpVersion("HTTP/1.1") {}
+	_method.first(UNKNOWN), _method.second(false),
+	_protocol.first("HTTP"), _protocol.second(true),
+	_domain.first("/"), _domain.second(true),
+	_port.first(80), _port.second(true),
+	_scriptName.second(false),
+	_path.second(false),
+	_queryString.second(false),
+	_fragment.second(false),
+	_httpVersion.first("HTTP/1.1"), _httpVersion.second(true),
+	_headers((NULL, false), (NULL, false)),
+	_body.second(false)
+	{}
 
 Request::Request(std::string &req) :
-	_method(UNKNOWN),
-	_protocol(HTTP),
-	_domain("/"),
-	_port(80),
-	_httpVersion("HTTP/1.1") { parser(req); }
+	_method.first(UNKNOWN), _method.second(false),
+	_protocol.first("HTTP"), _protocol.second(true),
+	_domain.first("/"), _domain.second(true),
+	_port.first(80), _port.second(true),
+	_scriptName.second(false),
+	_path.second(false),
+	_queryString.second(false),
+	_fragment.second(false),
+	_httpVersion.first("HTTP/1.1"), _httpVersion.second(true),
+	_headers((NULL, false), (NULL, false)),
+	_body.second(false)
+	{ parser(req); }
 
 Request::Request( const Request &src ) :
 	_method(src._method),
@@ -33,47 +47,18 @@ Request::~Request() {}
 
 /* **********************************************************/
 
-std::string Request::getMethod() const {
-	if (_method == GET)
-		return ("GET");
-	if (_method == POST)
-		return ("POST");
-	if (_method == DELETE)
-		return ("DELETE");
-	return ("UNKNOWN");
-}
-
-std::string	Request::getProtocol() const {
-	if (_protocol == HTTP)
-		return ("HTTP");
-	return ("UNKNOWN");
-}
-
-std::string Request::getDomain() const { return _domain; }
-
-std::string Request::getPort() const {
-	std::string port;
-	std::stringstream ss;
-	ss << _port;
-	ss >> port;
-	return port;
-}
-
-std::string Request::getScriptname() const { return _scriptName; }
-
-std::string Request::getPath() const { return _path; }
-
-std::string Request::getQuerystring() const { return _queryString; }
-
-std::string Request::getFragment() const { return _fragment; }
-
-std::string Request::getHttpversion() const { return _httpVersion; }
-
-std::map <std::string, std::string> Request::getHeaders() const {
-	return _headers;
-}
-
-std::string Request::getBody() const { return _body; }
+std::pair<method, bool>			Request::getMethod() const { return _method; }
+std::pair<std::string, bool>	Request::getProtocol() const { return _protocol; }
+std::pair<std::string, bool>	Request::getDomain() const { return _domain; }
+std::pair<unsigned int, bool>	Request::getPort() const { return _port; }
+std::pair<std::string, bool>	Request::getScriptname() const { return _scriptName; }
+std::pair<std::string, bool>	Request::getPath() const { return _path; }
+std::pair<std::string, bool>	Request::getQuerystring() const { return _queryString; }
+std::pair<std::string, bool>	Request::getFragment() const { return _fragment; }
+std::pair<std::string, bool>	Request::getHttpversion() const { return _httpVersion; }
+std::vector<std::pair<std::pair<std::string, bool>, std::pair<std::string, bool>>>
+								Request::getHeaders() const { return _headers; }
+std::pair<std::string, bool>	Request::getBody() const { return _body; }
 
 /* **********************************************************/
 
@@ -81,33 +66,45 @@ void Request::parser(std::string &req)
 {
 	setMethod(req);
 	setPath(req);
+	setHttpversion(req);
 }
 
 void Request::setMethod(std::string &req)
 {
 	size_t pos = 0;
 	std::string tmp;
-
 	pos = req.find(" ");
+
 	if (pos != std::string::npos)
 		tmp = req.substr(0, pos);
-	if (!tmp.compare("GET"))
-		_method = GET;
-	else if (!tmp.compare("POST"))
-		_method = POST;
-	else if (!tmp.compare("DELETE"))
-		_method = DELETE;
-	else
-		_method = UNKNOWN;
+	if (!tmp.compare("GET")) { _method.first = GET; _method.second = true; }
+	else if (!tmp.compare("POST")) { _method.first = POST; _method.second = true; }
+	else if (!tmp.compare("DELETE")) { _method.first = DELETE; _method.second = true; }
+	else { _method.first = UNKNOWN; _method.second = false;}
+
 	req.erase(0, pos + 1);
 }
 
 void Request::setPath(std::string &req)
 {
 	size_t pos = 0;
-
 	pos = req.find(" ");
-	_path = req.substr(0, pos);
+
+	_path.first = req.substr(0, pos);
+
+	req.erase(0, pos + 1);
+}
+
+void Request::setHttpversion(std::string &req)
+{
+	size_t pos = 0;
+	pos = req.find("\n");
+
+	_httpVersion.first = req.substr(0, pos);
+	if(!_httpVersion.first.compare("HTTP/1.1"))
+		_httpVersion.second = true;
+	else
+		_httpVersion.second = false;
 	req.erase(0, pos + 1);
 }
 
@@ -143,3 +140,10 @@ std::ostream &			operator<<( std::ostream & o, Request const & i )
 }
 
 /* **********************************************************/
+
+
+
+	// std::string port;
+	// std::stringstream ss;
+	// ss << _port;
+	// ss >> port;
