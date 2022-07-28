fa17a0ef951526603b0a4f8e3d459b9a0af5e71f
Yavuz Sönmez <contact@yavuzsonmez.com>
2022-07-28 16:01:30 +0200

    Request

--- inc/request.hpp
+++ inc/request.hpp
@@ -5,9 +5,11 @@
 # include <string>
 # include <vector>
 # include <sstream>
+# include <utility>
 # include "colors.hpp"
 
 enum method { GET, POST, DELETE, UNKNOWN };
+typedef std::vector<std::pair<std::pair<std::string, bool>, std::pair<std::string, bool>>> vect_headrs_pairs;
 
 class Request
 {
@@ -20,47 +22,44 @@ class Request
 		~Request();
 
 		//getters
-		std::pair<method, bool>					getMethod() const;
-		std::pair<std::string, bool>			getProtocol() const;
-		std::pair<std::string, bool>			getDomain() const;
-		std::pair<unsigned int, bool>			getPort() const;
-		std::pair<std::string, bool>			getScriptname() const;
-		std::pair<std::string, bool>			getPath() const;
-		std::pair<std::string, bool>			getQuerystring() const;
-		std::pair<std::string, bool>			getFragment() const;
-		std::pair<std::string, bool>			getHttpversion() const;
-		std::vector<std::pair<std::pair<std::string, bool>, std::pair<std::string, bool>>>
-												getHeaders() const;
-		std::pair<std::string, bool>			getBody() const;
+		std::pair <method, bool>				getMethod(void) const;
+		std::pair <std::string, bool>			getProtocol(void) const;
+		std::pair <std::string, bool>			getDomain(void) const;
+		std::pair <unsigned int, bool>			getPort(void) const;
+		std::pair <std::string, bool>			getScriptname() const;
+		std::pair <std::string, bool>			getPath(void) const;
+		std::pair <std::string, bool>			getQuerystring(void) const;
+		std::pair <std::string, bool>			getFragment(void) const;
+		std::pair <std::string, bool>			getHttpversion(void) const;
+		vect_headrs_pairs						getHeaders(void) const;
+		std::pair <std::string, bool>			getBody(void) const;
 
-		//main parser
-		void	parser(std::string &req);
-		//setters for parser
-		void	setMethod(std::string &req);
-		void	setProtocol(std::string &req);
-		void	setDomain(std::string &req);
-		void	setPort(std::string &req);
-		void	setScriptname(std::string &req);
-		void	setPath(std::string &req);
-		void	setQuerystring(std::string &req);
-		void	setFragment(std::string &req);
-		void	setHttpversion(std::string &req);
-		void	setHeaders(std::string &req);
-		void	setBody(std::string &req);
+		void									parser(std::string &req);
+		void									setMethod(std::string &req);
+		void									setProtocol(std::string &req);
+		void									setDomain(std::string &req);
+		void									setPort(std::string &req);
+		void									setScriptname(std::string &req);
+		void									setPath(std::string &req);
+		void									setQuerystring(std::string &req);
+		void									setFragment(std::string &req);
+		void									setHttpversion(std::string &req);
+		void									setHeaders(std::string &req);
+		void									setBody(std::string &req);
 
 	private:
-		std::pair<method, bool>				_method;
-		std::pair<std::string, bool>		_protocol;
-		std::pair<std::string, bool>		_domain;
-		std::pair<unsigned int, bool>		_port;
-		std::pair<std::string, bool>		_scriptName;
-		std::pair<std::string, bool>		_path;
-		std::pair<std::string, bool>		_queryString;
-		std::pair<std::string, bool>		_fragment;
-		std::pair<std::string, bool>		_httpVersion;
-		std::vector<std::pair<std::pair<std::string, bool>, std::pair<std::string, bool>>>
-											_headers;
-		std::pair<std::string, bool>		_body;
+
+		std::pair<method, bool>					_method;
+		std::pair<std::string, bool>			_protocol;
+		std::pair<std::string, bool>			_domain;
+		std::pair<unsigned int, bool>			_port;
+		std::pair<std::string, bool>			_scriptName;
+		std::pair<std::string, bool>			_path;
+		std::pair<std::string, bool>			_queryString;
+		std::pair<std::string, bool>			_fragment;
+		std::pair<std::string, bool>			_httpVersion;
+		vect_headrs_pairs						_headers;
+		std::pair<std::string, bool>			_body;
 };
 
 std::ostream &			operator<<( std::ostream & o, Request const & i );
