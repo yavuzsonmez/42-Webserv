94d463e56e854dfe83868a67f5d241f2fdba0c19
Jonathan Fritz <jfritz@1-B-5.42heilbronn.de>
2022-08-01 16:53:25 +0200

    Adding internal_keyvalue

--- inc/ConfigurationKey.hpp
+++ inc/ConfigurationKey.hpp
@@ -9,17 +9,24 @@
 enum ConfigurationKeyType { 
 	INDEX, 
 	SERVER_NAME,
-    PORT,
+    LISTEN,
     LOCATION,
     ROOT,
+    INVALID
 };
+
+/**
+ * The internal keypair used within the class to transport key and value. This is being isolated to avoid confusion between attribute key/value and
+ * internal handling of key and value
+ */
+typedef std::pair <std::string, std::string>	internal_keyvalue;
 
 /**
  * Usage: ConfigurationKey(key, value);
  * 
  * @note The configuration key represents a single entry (or a body containing values) in the configuration file.
  *       It is to be used in an array, and parses the given string-value automatically
- *       to the correct type in the constructor and sets it own type.
+ *       to the correct type in the constructor and sets it own configurationType.
  *       It can be nested, but only certain nestings are valid. (root in location block as example)
  * 
  * Currently, it supports following types:
@@ -27,10 +34,12 @@ enum ConfigurationKeyType {
  *  - port
  * 
  * To use it, split the key/value from the config_file from the first space and pass the first value as key and the rest as value.
- * Then it parses the value and by combining key and value it decides which type of configuration it is.
+ * Then it parses the value and by combining key and value it decides which configurationType it is.
  * You can find the available configuration types in the ConfigurationKey.hpp in the enums.
  * 
  * If there is no type which fits the value/key, it throws an exception and the program stops.
+ * 
+ * @note Internal handling in ConfigurationKey class is done with internal_keyvalue Pair typedef for readabiliy.
  * 
  */
 class ConfigurationKey {
@@ -43,13 +52,20 @@ class ConfigurationKey {
         // this is the initializer taking the key and the raw value from the configuration file
         ConfigurationKey(std::string key, std::string value);
 
-        // Attributes
+        // General Attributes
 
         std::string key;
         std::string value;
 
+        // Configuration type
+        ConfigurationKeyType configurationType;
+
+        // Attributes of the configuration types
         std::vector <std::string> indexes; // indexes, if type is INDEX. sorted by relevance and position within the key.
+        std::string root; // returns the path of the root
+        std::string location; // returns the locationpath of the location
+        std::vector <unsigned int> ports; // returns the ports which are being listened to by the listener handler
     private:
-
-
+        ConfigurationKeyType detectConfigurationType(internal_keyvalue raw);
+        bool isServerNameKeyType(internal_keyvalue raw);
 }
