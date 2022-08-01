599a8f1ea021446837586d28314df1af14d26517
Jonathan Fritz <jfritz@1-B-5.42heilbronn.de>
2022-08-01 16:53:25 +0200

    Adding internal_keyvalue

--- src/configuration_key/ConfigurationKey.cpp
+++ src/configuration_key/ConfigurationKey.cpp
@@ -1,1 +1,39 @@
 #include "../../inc/ConfigurationKey.hpp"
+
+/**
+ * Default constructor
+ */
+ConfigurationKey::ConfigurationKey() {
+    throw{"Unsupported constructor. Use key value constructor instead!"};
+}
+
+ConfigurationKey::ConfigurationKey( const ConfigurationKey &src ) {
+
+}
+
+ConfigurationKey::~ConfigurationKey() {
+
+}
+
+ConfigurationKey & ConfigurationKey::operator = (const ConfigurationKey &src) {
+	return (*this);
+}
+
+/**
+ * Actual constructor of configuration key class.
+ * Will take raw key and value and convert it in the internal_keyvalue for better handling within the class.
+ * This calls detectConfigurationType.
+ */
+ConfigurationKey::ConfigurationKey(std::string key, std::string value) {
+
+}
+
+/**
+ * @param Returns the correct configuration key based on the key and value.
+ * 
+ * If the key is invalid or not yet implemented, it returns INVALID. This should
+ * be treated as fatal error.
+ */
+ConfigurationKeyType ConfigurationKey::detectConfigurationType(internal_keyvalue raw) {
+    return INVALID;
+}
