e45b834a77a6ecb5999ea84f6ed014d0b6959b71
Jonathan Fritz <fritz@nosc.io>
2022-08-01 09:52:33 +0200

    Implementing different strip from str

--- src/utility/strip_from_str.cpp
+++ src/utility/strip_from_str.cpp
@@ -1,36 +1,40 @@
 #include <string>
 #include <iostream>
+#include <sstream>
+
 
 //Remove in file_content everything between start (included) to end (excluded)
 //Remove comment (starting with #) from file content
 void strip_from_str(std::string &file_content, const char start, const char end)
 {
-	size_t start_index = file_content.find(start);
-	size_t end_index = file_content.find(end);
+	size_t start_index;
+	size_t end_index;
+	std::istringstream iss(file_content);
+	std::string result;
 
-	std::string::iterator it_s = file_content.begin() + start_index;
-	std::string::iterator it_e = file_content.begin() + end_index;
-
-	while ( start_index != std::string::npos)
+	for (std::string line; std::getline(iss, line); )
 	{
-		file_content.replace(it_s, it_e, "\n");
-		start_index = file_content.find(start);
-		end_index = file_content.find(end);
-		it_s = file_content.begin() + start_index;
-		it_e = file_content.begin() + end_index;
+		if (line.find("#") == -1)
+			result += line + "\n";
+			continue;
+		start_index = line.find('#');
+		std::cout << start_index << " to " << line.length() << std::endl;
+		std::cout << line << std::endl;
+		line.replace(start_index, line.length(), "");
+		std::cout << line << std::endl;
+		result += line + "\n";
 	}
-
+	file_content = result;
 }
 
 
-// int	main(int argc, char**argv)
-// {
+int	main(int argc, char**argv)
+{
 
-// 	std::string config_file = "hello world; #q powngfpoqwngpo\nlisten	8000;\n#hihihih;";
-
-// 	std::cout << config_file << std::endl;
-// 	std::cout << "____________________" << std::endl;
-// 	strip_from_str(config_file, '#', '\n');
-// 	std::cout << config_file << std::endl;
-// 	return (0);
-// }
+	std::string config_file = "TEST MESSAGE\nTEST MESSAGE2\n#TEST MESSAGE3\nTEST MESSAGE4";
+	std::cout << config_file << std::endl;
+	std::cout << "____________________" << std::endl;
+	strip_from_str(config_file, '#', '\n');
+	std::cout << config_file << std::endl;
+	return (0);
+}
