405adda33ebb8efb7302269efebfa51a82dc96f6
Yavuz Sönmez <contact@yavuzsonmez.com>
2022-08-02 12:12:38 +0200

    add http_headers.hpp

---
+++ inc/http_headers.hpp
@@ -0,0 +1,26 @@
+
+#ifndef HTTP_HEADERS_HPP
+# define HTTP_HEADERS_HPP
+
+// HTTP headers that Webserv support
+
+#define Accept_Encoding "Accept-Encoding"
+#define Accept_Language "Accept-Language"
+#define Allow "Allow"
+#define Authorization "Authorization"
+#define Content_Language "Content-Language"
+#define Content_Length "Content-Length"
+#define Content_Location "Content-Location"
+#define Content_Type "Content-Type"
+#define Host "Host"
+#define Last_Modified "Last-Modified"
+#define Location "Location"
+#define Referer "Referer"
+#define Retry_After "Retry-After"
+#define Server "Server"
+#define Transfer_Encoding "Transfer-Encoding"
+#define User_Agent "User-Agent"
+#define WWW_Authenticate "WWW-Authenticate"
+#define X_Header "X-*	Header"
+
+#endif
