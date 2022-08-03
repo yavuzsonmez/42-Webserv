a301c1c246dbd8740857d75c8a609b94a873860a
Jonathan Fritz <jfritz@1-B-5.42heilbronn.de>
2022-08-03 17:15:48 +0200

    Add get_server_block_helper

---
+++ src/utility/get_server_block.cpp
@@ -0,0 +1,68 @@
+#include "../../inc/utility.hpp"
+#include <vector>
+#include <string>
+
+/**
+ * Jumps to the next server prefix to get the server block.
+ * A server block always starts with the line
+ * server {
+ * and nothing else will be accepted as a prefix for a serverblock.
+ * 
+ * Returns the line of the start of the next server block.
+ * Takes in the last read line and skips all until it hits line positon
+ * 
+ * @param filecontent of the fiel
+ * @param position of the line to skip to or -1 if the end was reached
+ * 
+ * @note We start counting at 0!
+ */
+int getNextServerPrefix(std::string file_content, int position) {
+    std::istringstream iss(file_content);
+	std::string result;
+    int i = 0;
+    // skip all characters before position
+	for (std::string line; std::getline(iss, line); )
+	{
+        if (position >= i)
+        {
+            i++;
+            continue;
+        }
+        if (line == "server {")
+            return i;
+        i++;
+	};
+    return -1;
+}
+
+/**
+ * Counts how many server blocks are in the configuration file
+ */
+int getCoutnOfServerBlocks(std::string file_content) {
+
+}
+
+/**
+ * Returns the server blocks as strings.
+ * Each block returned represents a server block.
+ * @param file_content
+ * @return server_blocks as string vector
+ */
+std::vector<std::string> get_server_blocks(std::string &file_content)
+{
+    std::vector<std::string> servers;
+
+
+    return servers;
+}
+
+
+
+// int main() {
+//     int line = getNextServerPrefix("Linne Line Line Line Linne\nserver {\niyasgdfoiugyasgyu\n\nserver {\negtriryg87w4eo57r", 0);
+//     std::cout << line << std::endl;
+//     int line2 = getNextServerPrefix("Linne Line Line Line Linne\nserver {\niyasgdfoiugyasgyu\n\nserver {\negtriryg87w4eo57r", line);
+//     std::cout << line2 << std::endl;
+//     int line3 = getNextServerPrefix("Linne Line Line Line Linne\nserver {\niyasgdfoiugyasgyu\n\nserver {\negtriryg87w4eo57rLinne Line Line Line Linne\nserver {\niyasgdfoiugyasgyu\n\nserver {\negtriryg87w4eo57r", line2);
+//     std::cout << line3 << std::endl;
+// }
