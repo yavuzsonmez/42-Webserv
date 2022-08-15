842ec567be9709c0a9677b8ace401cfb16a8ff08
Jonathan Fritz <jfritz@1-B-5.42heilbronn.de>
2022-08-15 10:10:43 +0200

    Addng Get all indexes function to server block class

--- inc/ServerBlock.hpp
+++ inc/ServerBlock.hpp
@@ -12,7 +12,7 @@
  * 
  * Holds multiple configuration keys
  * 
- * Also has functions to easily receive all ports and server names.
+ * Also has functions to easily receive all ports and server names and indexes (ordered)
  */
 class ServerBlock
 {
@@ -24,6 +24,7 @@ class ServerBlock
         std::vector<ConfigurationKey> configurationKeys;
         std::vector<std::string> getAllServerNames();
         std::vector<unsigned int> getAllServerPorts();
+        std::vector<std::string> getAllIndexes();
     private:
         void addConfigurationKey(ConfigurationKey configurationKey);
         DebuggerPrinter debugger;
