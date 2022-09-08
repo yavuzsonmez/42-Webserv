43dc6099abb47f73d9ed2848cf77e2488a0526f7
Jonathan Fritz <jonathanfritz@AirvonJonathan2.fritz.box>
2022-09-08 16:55:28 +0200

    New constructor for invalid config exception

--- inc/config_file/InvalidConfigurationFile.hpp
+++ inc/config_file/InvalidConfigurationFile.hpp
@@ -5,7 +5,7 @@
 class InvalidConfigurationFile : public std::exception {
 			public:
 				virtual const char* what() const throw();
-				virtual const char* what(char *message) const throw();
+				virtual const char* what(char* message, int current_line, char* raw_input) const throw();
 };
 
 #endif
--- src/config_file/InvalidConfigurationFile.cpp
+++ src/config_file/InvalidConfigurationFile.cpp
@@ -1,4 +1,6 @@
 #include "../../inc/config_file/InvalidConfigurationFile.hpp"
+#include "../../inc/debugger/DebuggerPrinter.hpp"
+#include "../../inc/utility/colors.hpp"
 #include <exception>
 
 /**
@@ -10,12 +12,14 @@ const char* InvalidConfigurationFile::what() const throw() {
 	return "Configuration File is invalid. Server cannot start.";
 }
 
-/**
- * @brief Returns custom error message
- * 
- * @param message 
- * @return const char* 
- */
-const char* InvalidConfigurationFile::what(char *message) const throw() {
+const char* InvalidConfigurationFile::what(char *message, int current_line, char *raw_input)  const throw() {
+	USE_DEBUGGER;
+	std::cout << R << "----------------- FAILED -----------------" << Reset << std::endl;
+	std::cout << R << "Encountered a problem with configuration on line " << (current_line + 1) << Reset << ":" << std::endl;
+	std::cout << (current_line) << " ..." <<  std::endl;
+	std::cout << (current_line + 1) << " " << Y << raw_input << Reset << std::endl;
+	std::cout << (current_line + 2) << " ..." <<  std::endl;
+	debugger.error(message);
+	std::cout << R << "The parser did not continue after finding this error. There may be more." << std::endl;
 	return message;
 }
