0b9dd9559c2d74e7df2e0054a3d9e94a6b8f23c8
Philipp Weinstock <pweinsto@1-B-4.42heilbronn.de>
2022-10-05 17:31:10 +0200

    SERVER_PORT

--- src/http/Cgi.cpp
+++ src/http/Cgi.cpp
@@ -14,12 +14,13 @@ CGI::CGI(/*Request &request*/Request request, ServerBlock &config, std::string p
 		_query_parameters = split_on_delimiter(query, '&');
 	}
 
+	// when SERVER_SOFTWARE OR SERVER_NAME is uncommented, there is a weird security warning.
 	//_env["SERVER_SOFTWARE"] = "webserv";											//The name and version of the information server software answering the request (and running the gateway). Format: name/version 
 	//_env["SERVER_NAME"] = _config.getAllServerNames().front();					//The server's hostname, DNS alias, or IP address as it would appear in self-referencing URLs.
 	//_env["GATEWAY_INTERFACE"] = "CGI/1.1";										//The revision of the CGI specification to which this server complies. Format: CGI/revision
 	_env["SERVER_PROTOCOL"] = _request.getProtocol().first;							//The name and revision of the information protcol this request came in with. Format: protocol/revision
-	_env["SERVER_PORT"] = _request.getPort().first;									//The port number to which the request was sent.
-	//_env["REQUEST_METHOD"] = _request.getMethod().first;							//The method with which the request was made. For HTTP, this is "GET", "HEAD", "POST", etc.
+	_env["SERVER_PORT"] = to_str(_request.getPort().first);									//The port number to which the request was sent.
+	// _env["REQUEST_METHOD"] = _request.getMethod().first;							//The method with which the request was made. For HTTP, this is "GET", "HEAD", "POST", etc.
 	_env["PATH_INFO"] = _request.getPath().first;									//The extra path information, as given by the client. In other words, scripts can be accessed by their virtual pathname, followed by extra information at the end of this path. The extra information is sent as PATH_INFO. This information should be decoded by the server if it comes from a URL before it is passed to the CGI script.
 	_env["PATH_TRANSLATED"] = _request.getPath().first;								//The server provides a translated version of PATH_INFO, which takes the path and does any virtual-to-physical mapping to it.
 	_env["SCRIPT_NAME"] = "";														//A virtual path to the script being executed, used for self-referencing URLs.
@@ -29,8 +30,9 @@ CGI::CGI(/*Request &request*/Request request, ServerBlock &config, std::string p
 	_env["AUTH_TYPE"] = "";															//If the server supports user authentication, and the script is protects, this is the protocol-specific authentication method used to validate the user.
 	_env["REMOTE_USER"] = "";														//If the server supports user authentication, and the script is protected, this is the username they have authenticated as. 
 	_env["REMOTE_IDENT"] = "";														//If the HTTP server supports RFC 931 identification, then this variable will be set to the remote user name retrieved from the server. Usage of this variable should be limited to logging only. 
-	_env["CONTENT_TYPE"] = "";	//need the content-type from request header			//For queries which have attached information, such as HTTP POST and PUT, this is the content type of the data.
+	_env["CONTENT_TYPE"] = "20000";	//need the content-type from request header			//For queries which have attached information, such as HTTP POST and PUT, this is the content type of the data.
 	_env["CONTENT_LENGTH"] = _request.getBody().first.length();						//The length of the said content as given by the client.
+	_env["REDIRECT_STATUS"] = "500";
 }
 
 CGI::~CGI()
