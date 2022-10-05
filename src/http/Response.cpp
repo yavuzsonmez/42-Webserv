0cfb39b60b341be2a38a160da199251d2dfd08f8
Philipp Weinstock <pweinsto@1-B-4.42heilbronn.de>
2022-10-05 18:00:20 +0200

    clean2

--- src/http/Response.cpp
+++ src/http/Response.cpp
@@ -15,35 +15,6 @@ Response::~Response(void)
 
 }
 
-// void	Response::process_request(void)
-// {
-// 	std::cout << "request: " << _request.getScript().first << std::endl;
-// 	_vec_loc = _config.getConfigurationKeysWithType(LOCATION);
-// 	if (_request.getScript().first.empty())
-// 	{
-// 		_location = Location(_config);
-// 	}
-// 	else
-// 	{
-// 		int flag = 0;
-// 		for (std::vector<ConfigurationKey>::iterator it = _vec_loc.begin(); it != _vec_loc.end(), it++)
-// 		{
-// 			if (_request.getScript().first == (*it).location);
-// 			{
-// 				_location = Location(*it);
-// 				flag = 1;
-// 				break;
-// 			}
-// 		}
-// 		if (!flag)
-// 		{
-// 			_location = Location();
-// 		}
-// 	}
-	
-// 	if(_location._index.substr(fn.find_last_of(".") + 1) == "conf")
-// }
-
 void	Response::set_protocol(std::string protocol){_protocol = protocol;}
 void	Response::set_status_code(std::string status_code){_status_code = status_code;}
 void	Response::set_status_text(std::string status_text){_status_text = status_text;}
@@ -77,17 +48,6 @@ void	Response::create_response(void)
 		_response += "content-type: " + _content_type + "\r\n";
 	if (!_content_length.empty())
 		_response += "content-length: " + _content_length + "\r\n";
-	// if (!_html.empty())
-	// 	_response += "\r\n" + _html;
-	// if (!_plain.empty())
-	// 	_response += "\r\n" + _plain;
-	// if (!_image.empty())
-	// 	_response += "\r\n" + _image;
-	// if (!_transfer_encoding.empty())
-	// {
-	// 	_response += "transfer-encoding: " + _transfer_encoding + "\r\n\r\n";
-	// 	return ;
-	// }
 	if (!_body.empty())
 		_response += "\r\n" + _body;
 }
@@ -106,58 +66,3 @@ std::string	Response::get_file_format(void)
 		return ("video/mp4");
 	return("unknown");
 }
-
-void	Response::test_text(void)
-{
-	_protocol = "HTTP/1.1";
-	_status_code = "200";
-	_status_text = "OK";
-
-	_server = "PetRoulette";
-	_content_type = "text/plain";
-	_body = "Hello World";
-	_content_length = to_str(_body.length());
-	create_response();
-}
-
-void	Response::test_html(void)
-{
-	_protocol = "HTTP/1.1";
-	_status_code = "200";
-	_status_text = "OK";
-
-	_server = "PetRoulette";
-	_content_type = "text/html";
-	_body = get_file_content("html/index.html");
-	_content_length = to_str(_body.length());
-	create_response();
-}
-
-void	Response::test_image(void)
-{
-	_protocol = "HTTP/1.1";
-	_status_code = "200";
-	_status_text = "OK";
-
-	_server = "PetRoulette";
-	//_content_type = "image/png";
-	_body = get_file_content("images/Rotating_earth_(large).gif");
-	_content_type = get_file_format();
-	_content_length = to_str(_body.length());
-
-	create_response();
-}
-
-// void	Response::test_cgi(ServerBlock &config)
-// {
-// 	_protocol = "HTTP/1.1";
-// 	_status_code = "200";
-// 	_status_text = "OK";
-// 	CGI	cgi(_request, config);
-// 	cgi.execute();
-// 	_body = cgi.get_buf();
-// 	_server = "PetRoulette";
-// 	_content_type = "text/html";
-// 	_content_length = to_str(_body.length());
-// 	create_response();
-// }
