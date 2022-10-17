aac39372d9c45711dbff9b15681837bf7937f477
pweinstock <37242263+pweinstock@users.noreply.github.com>
2022-10-17 13:02:01 +0200

    svae

--- cgi-bin/upload.py
+++ cgi-bin/upload.py
@@ -8,16 +8,8 @@ fileitem = form['filename']
 if fileitem.filename:
    # strip leading path from file name to avoid
    # directory traversal attacks
-   fn = os.path.basename(fileitem.filename.replace("\", "/" ))
+   fn = os.path.basename(fileitem.filename.replace("\\", "/" ))
    open('/tmp/' + fn, 'wb').write(fileitem.file.read())
    message = 'The file "' + fn + '" was uploaded successfully'
 else:
    message = 'No file was uploaded'
-print """\
-Content-Type: text/html\n
-<html>
-<body>
-   <p>%s</p>
-</body>
-</html>
-""" % (message,)
