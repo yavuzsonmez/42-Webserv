ae229e45b4a8a52084af89a61f198d920ea0082b
Philipp Weinstock <pweinsto@2-A-2.42heilbronn.de>
2022-10-18 11:23:08 +0200

    tuesday

--- cgi-bin/upload.py
+++ cgi-bin/upload.py
@@ -1,15 +1,17 @@
-#!/usr/bin/python
-import cgi, os
-import cgitb; cgitb.enable()
-form = cgi.FieldStorage()
-# Get filename here.
-fileitem = form['filename']
-# Test if the file was uploaded
-if fileitem.filename:
-   # strip leading path from file name to avoid
-   # directory traversal attacks
-   fn = os.path.basename(fileitem.filename.replace("\\", "/" ))
-   open('/tmp/' + fn, 'wb').write(fileitem.file.read())
-   message = 'The file "' + fn + '" was uploaded successfully'
-else:
-   message = 'No file was uploaded'
+import cgi, fileinput, os, sys
+
+try:
+	message = 'what?!'
+	message = cgi.FieldStorage()
+	fileitem = parsed['filename']
+except:
+	message = 'No file was uploaded'
+
+print("""\
+Content-Type: text/html\r\n\r\n
+<html>
+<body>
+   <p>%s</p>
+</body>
+</html>
+""" % (message))
