3eef59f596c7849c0b3a0e47c5e45afea81fed65
Jonathan Fritz <jonathanfritz@MacBook-Air-von-Jonathan-2.local>
2022-11-19 11:54:40 +0100

    Changing utility function not to throw anymore

--- src/utility/get_file_content_for_request.cpp
+++ src/utility/get_file_content_for_request.cpp
@@ -1,9 +1,18 @@
 
 #include "../../inc/utility/utility.hpp"
 
-std::string get_file_content(std::string path)
+/**
+ * @brief Get the file content for a path. 
+ * 
+ * @param path 
+ * @return std::string 
+ * @throw 404 if file could not be opened
+ */
+std::string get_file_content_for_request(std::string path)
 {
 	std::ifstream f(path); //taking file as inputstream
+	if (!f)
+		throw (404);
 	std::string str;
 
 	if(f) {
