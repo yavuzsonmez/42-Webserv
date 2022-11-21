23053bb4cad29bad2e8e2d13cf8c8962a37864d4
Jonathan Fritz <jonathanfritz@MacBook-Air-von-Jonathan-2.local>
2022-11-21 13:03:14 +0100

    Now caching error pages on 503

--- src/utility/get_file_content.cpp
+++ src/utility/get_file_content.cpp
@@ -14,6 +14,22 @@ std::string get_file_content(std::string path)
 	return (str);
 }
 
+/**
+ * @brief Get the file content. Caches the file content, if it was already read.
+ * 
+ * @param path 
+ * @return std::string 
+ */
+std::string get_file_content_cached(std::string path)
+{
+	static std::map<std::string, std::string> file_content_cache;
+
+	if (file_content_cache.find(path) == file_content_cache.end()) {
+		file_content_cache[path] = get_file_content(path);
+	}
+	return file_content_cache[path];
+}
+
 // int	main(int argc, char**argv)
 // {
 // 	(void)argc;
