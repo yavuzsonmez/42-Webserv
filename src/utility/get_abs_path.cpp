4b655e6c9f7cc2efbc11a5c90012dca934c950bf
Jonathan Fritz <jonathanfritz@AirvonJonathan2.fritz.box>
2022-11-22 14:53:59 +0100

    More fixes in env

--- src/utility/get_abs_path.cpp
+++ src/utility/get_abs_path.cpp
@@ -1,6 +1,12 @@
 #include <unistd.h>
 #include <string>
 
+/**
+ * @brief Get the absolute path of a file WITHOUT any arguments if they were provided (e.g. index.php?arg1=1&arg2=2)
+ * 
+ * @param path 
+ * @return std::string 
+ */
 std::string	get_abs_path(const std::string & path)
 {
 	std::string str;
@@ -11,5 +17,9 @@ std::string	get_abs_path(const std::string & path)
 		free(pwd);
 	}
 	str += path.substr(1);
+	// remvoe arguments from str
+	size_t pos = str.find('?');
+	if (pos != std::string::npos)
+		str.erase(pos);
 	return str;
 }
