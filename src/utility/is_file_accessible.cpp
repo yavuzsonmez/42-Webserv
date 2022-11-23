faab8b7285f292d5f6bb0e1a2955abc4eac812ec
Jonathan Fritz <jonathanfritz@MacBook-Air-von-Jonathan-2.fritz.box>
2022-11-23 10:32:30 +0100

    Not using ifstream anymore

--- src/utility/is_file_accessible.cpp
+++ src/utility/is_file_accessible.cpp
@@ -3,10 +3,7 @@
 //check if file exists and permission
 bool is_file_accessible(std::string const &path)
 {
-	std::ifstream my_file(path);
-	if (!my_file.fail() && my_file.good())
-		return true;
-	return false;
+	return (access(path.c_str(), F_OK) != -1);
 }
 
 
