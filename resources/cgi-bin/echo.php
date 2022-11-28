89bdea39955ce59e5499d34ea80ce8b3003947b3
Jonathan Fritz <fritz@nosc.io>
2022-11-28 11:54:02 +0100

    Style the landing page a bit

--- resources/cgi-bin/echo.php
+++ resources/cgi-bin/echo.php
@@ -2,22 +2,29 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>HELLO</title>
-	<?php
-		// print all headers with foreach
-		echo "<br><b>HEADER LIST</b>" . "<br>";;
-		foreach (getallheaders() as $name => $value) {
-			echo "$name: $value" . "<br>";
-		}
-	?>
-</head>
-<body>
-	<h1>Firstname: <?php echo("TEST");?></h1>
-	<h1>Lastname: <?php echo $_GET['Lastname'];?></h1>
-	<h1>Age: <?php echo $_GET['Age'];?></h1>
-	<h1>Home: <?php echo $_GET['Home'];?></h1>
-</body>
-<?php phpinfo(); ?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<div class="card" style="width: 100%;">
+  <div class="card-body">
+    <h5 class="card-title">Welcome to the PetRoulette Webserver!</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Following links may be usefule to you:</h6>
+    <div class="card">
+		<div class="card-body">
+			<a href="/testargs.php?Firstname=Handsome&Lastname=Evaluator&Age=Young&Home=42">Argument test</a>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card-body">
+			<a href="/info.php">Info PHP</a>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card-body">
+			<a href="/cms/cms.php">CMS PHP (create upload folder before uploading files please!)</a>
+		</div>
+	</div>
+	<br>
+  </div>
+</div>
+
 </html>
