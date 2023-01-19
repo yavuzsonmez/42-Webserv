2b4f87f79547b75b678f8954464b3699521c14bd
Jonathan Fritz <fritz@nosc.io>
2023-01-20 00:30:42 +0100

    Cookie page more fancy

--- resources/cms/cookie.php
+++ resources/cms/cookie.php
@@ -1,8 +1,49 @@
 <?php
-// set a cookie
-setcookie("Firstname", "John", time()+3600, "/","", 0);
-setcookie("Lastname", "Doe", time()+3600, "/", "",  0);
-setcookie("Age", "36", time()+3600, "/", "",  0);
-setcookie("Home", "USA", time()+3600, "/", "",  0);
+		// set a cookie
+		setcookie("Firstname", "Handsome", time()*5, "/","", 0);
+		setcookie("Lastname", "Evaluator", time()*5, "/", "",  0);
+		setcookie("Age", "Young", time()+3600, "/", "",  0);
+		setcookie("Home", "42", time()+3600, "/", "",  0);
+		session_start();
 ?>
-<h1>COOKIE WAS SET ON YOUR BROWSER!</h1>
+<title>Cookie Display</title>
+<style>
+	/* Add a background color */
+	body {
+		background-color: #f2f2f2;
+	}
+	/* Style the cookie tiles */
+	.cookie-tile {
+		background-color: #cccccc;
+		color: white;
+		font-weight: bold;
+		padding: 10px;
+		margin: 10px;
+		display: inline-block;
+	}
+</style>
+<body>
+	<h1>HELLO EVALUATOR! COOKIE WAS SET ON YOUR BROWSER!</h1>
+	<h2>Also, if you new refresh this page, you will see the cookie values below:</h2>
+
+<script>
+	// display all cookies
+	var cookies = document.cookie.split(";");
+	// remove php session cookie from array
+	cookies.pop();
+	for (var i = 0; i < cookies.length; i++) {
+		var cookie = cookies[i];
+		var eqPos = cookie.indexOf("=");
+		var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+		var value = cookie.substr(eqPos + 1);
+		// create a div for each cookie and add the name and value
+		var div = document.createElement("div");
+		div.classList.add("cookie-tile");
+		div.innerHTML = name + " = " + value;
+		// add the div to the body
+		document.body.appendChild(div);
+	}
+</script>
+
+<p>BTW: We also started a session. Your session is <?php echo session_id(); ?></p>
+</body>
